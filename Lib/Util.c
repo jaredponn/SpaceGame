c4b9c2c919721b9f279b87308f00e329b49c0265
jaredponn <jpon1234@gmail.com>
2018-06-29 23:30:31 -0600

    added a custom sting concat function

--- Lib/Util.c
+++ Lib/Util.c
@@ -1,11 +1,19 @@
 #include "Util.h"
 #include <stdio.h>
+#include <string.h>
 
-void* safe_malloc(size_t n) {
+void* safe_malloc(const size_t n) {
         void* p = malloc(n);
         if (p == NULL) {
                 fprintf(stderr, "Fatal: failed to allocate %zu bytes.\n", n);
                 abort();
         }
         return p;
+}
+
+char* UTI_strcat(const char* str0, const char* str1) {
+        char* newstr = malloc(sizeof(char) * (strlen(str0) + strlen(str1) + 1));
+        strcat(newstr, str0);
+        strcat(newstr, str1);
+        return newstr;
 }
--- Lib/Util.h
+++ Lib/Util.h
@@ -17,4 +17,8 @@
 // -----------------------------------------
 
 // mallocs with error handling
-void* safe_malloc(size_t n);
+void* safe_malloc(const size_t n);
+
+// concats two strings by copying them to a new malloc'ed buffer. Remember to
+// call "free" for the new string as they are allocated on the heap.
+char* UTI_strcat(const char*, const char*);
