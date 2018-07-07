d4a9bb8444b3b5fe7127a45014f78b1d62b2d8b6
jaredponn <jpon1234@gmail.com>
2018-07-06 21:28:31 -0600

    made a generic runTest function

--- UnitTesting/Run_Tests.c
+++ UnitTesting/Run_Tests.c
@@ -1,17 +1,57 @@
 #include <check.h>
 #include <stdlib.h>
 
+// Component test includes
+#include "Src/tests/CPT_Appearance_Tests.h"
+
+// Lib tests includes
 #include "Lib/tests/ComponentManager_Tests.h"
 #include "Lib/tests/Freelist_Test.h"
 #include "Lib/tests/Stack_Tests.h"
 #include "Lib/tests/UTI_strcatTest.h"
 #include "Lib/tests/Vector_Tests.h"
 
-int libTests(void) {
-        int number_failed;
-        Suite* s;
-        SRunner* sr;
+// -----------------------------------------
+//    Function declarations
+// -----------------------------------------
 
+// generic run test function
+int runTest(int (*fp)(Suite*, SRunner*));
+
+// lib test suite
+int libTests(Suite* s, SRunner* sr);
+
+// cpt test suite
+
+// -----------------------------------------
+//    Generic run test function
+// -----------------------------------------
+
+int runTest(int (*fp)(Suite*, SRunner*)) {
+        int number_failed = 0;
+        Suite* s = NULL;
+        SRunner* sr = NULL;
+
+        number_failed = (*fp)(s, sr);
+
+        srunner_free(sr);
+
+        return number_failed;
+}
+
+// -----------------------------------------
+//    Test suites
+// -----------------------------------------
+
+int cptTests(Suite* s, SRunner* sr) {
+        s = appearances_test_suite();
+        sr = srunner_create(s);
+
+        srunner_run_all(sr, CK_NORMAL);
+        return srunner_ntests_failed(sr);
+}
+
+int libTests(Suite* s, SRunner* sr) {
         // starts withe the vector test suite
         s = vector_test_suite();
         sr = srunner_create(s);
@@ -23,16 +63,18 @@ int libTests(void) {
         srunner_add_suite(sr, componentmanager_test_suite());
 
         srunner_run_all(sr, CK_NORMAL);
-        number_failed = srunner_ntests_failed(sr);
-        srunner_free(sr);
 
-        return number_failed;
+        return srunner_ntests_failed(sr);
 }
+// -----------------------------------------
+//    Main
+// -----------------------------------------
 
 int main(void) {
         int number_failed = 0;
 
-        number_failed += libTests();
+        number_failed += runTest(&libTests);
+        number_failed += runTest(&cptTests);
 
         return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
