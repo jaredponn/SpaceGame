7e6d89b6afad2e639a7fbb14eb711056a62adaba
jaredponn <jpon1234@gmail.com>
2018-07-06 18:14:09 -0600

    refactored the unit tests

--- Src/Engine/Components/Appearance.c
+++ Src/Engine/Components/Appearance.c
@@ -1,12 +1,11 @@
 #include "Appearance.h"
-#include <math.h>
 
 VECTOR_DEFINE(Appearance, Appearance)
 COMPONENT_MANAGER_DEFINE(Appearance, Appearance)
 
 void CPT_modifySdlRectPosition(SDL_Rect* rect, const Position pos) {
-        int nx = round(pos.x);
-        int ny = round(pos.y);
+        int nx = (int)pos.x;
+        int ny = (int)pos.y;
 
         rect->w = rect->w + nx;
         rect->h = rect->h + ny;
--- UnitTesting/Run_Tests.c
+++ UnitTesting/Run_Tests.c
@@ -1,16 +1,16 @@
 #include <check.h>
 #include <stdlib.h>
 
-#include "ComponentManager_Tests.h"
-#include "Freelist_Test.h"
-#include "Stack_Tests.h"
-#include "UTI_strcatTest.h"
-#include "Vector_Tests.h"
+#include "Lib/tests/ComponentManager_Tests.h"
+#include "Lib/tests/Freelist_Test.h"
+#include "Lib/tests/Stack_Tests.h"
+#include "Lib/tests/UTI_strcatTest.h"
+#include "Lib/tests/Vector_Tests.h"
 
-int main(void) {
+int libTests(void) {
         int number_failed;
-        Suite *s;
-        SRunner *sr;
+        Suite* s;
+        SRunner* sr;
 
         // starts withe the vector test suite
         s = vector_test_suite();
@@ -25,5 +25,14 @@ int main(void) {
         srunner_run_all(sr, CK_NORMAL);
         number_failed = srunner_ntests_failed(sr);
         srunner_free(sr);
+
+        return number_failed;
+}
+
+int main(void) {
+        int number_failed = 0;
+
+        number_failed += libTests();
+
         return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
