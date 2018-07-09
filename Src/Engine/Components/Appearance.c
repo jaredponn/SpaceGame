a36dd958eebce72712a4635d85db27be6ae286a4
jaredponn <jpon1234@gmail.com>
2018-07-08 20:58:57 -0600

    changed to use const and pass by pointer instead of copy

--- Src/Engine/Components/Appearance.c
+++ Src/Engine/Components/Appearance.c
@@ -10,13 +10,12 @@ COMPONENT_MANAGER_DEFINE(Appearance, Appearance)
 //    Internal declarations
 // -----------------------------------------
 
-static Position position_round(const Position val);
-
+static Position position_round(const Position* const val);
 // -----------------------------------------
 //    Function implementations
 // -----------------------------------------
 
-void CPT_setSdlRectPosition(SDL_Rect* rect, const Position pos) {
+void CPT_setSdlRectPosition(SDL_Rect* rect, const Position* const pos) {
         Position roundedPos = position_round(pos);
 
         int nx = roundedPos.x;
@@ -26,8 +25,9 @@ void CPT_setSdlRectPosition(SDL_Rect* rect, const Position pos) {
         rect->y = ny;
 }
 
-void CPT_modifySdlRectPosition(SDL_Rect* rect, const Position pos) {
+void CPT_modifySdlRectPosition(SDL_Rect* rect, const Position* const pos) {
         Position roundedPos = position_round(pos);
+
         int roundedx = roundedPos.x;
         int roundedy = roundedPos.y;
 
@@ -36,6 +36,6 @@ void CPT_modifySdlRectPosition(SDL_Rect* rect, const Position pos) {
 }
 
 // internals
-static Position position_round(const Position val) {
-        return (Position){.x = (int)val.x, .y = (int)val.y};
+static Position position_round(const Position* const val) {
+        return (Position){.x = (int)val->x, .y = (int)val->y};
 }
--- Src/Engine/Components/Appearance.h
+++ Src/Engine/Components/Appearance.h
@@ -9,13 +9,14 @@ typedef struct Appearance {
         SDL_Texture* texture;
         SDL_Rect srcrect;  // source rectangle
         SDL_Rect dstrect;  // destination rectangle
+        double angle;      // angle to rotate the texture
 } Appearance;
 
 VECTOR_DECLARE(Appearance, Appearance)
 COMPONENT_MANAGER_DECLARE(Appearance, Appearance)
 
 // sets the position of the SDL_Rect
-void CPT_setSdlRectPosition(SDL_Rect*, const Position);
+void CPT_setSdlRectPosition(SDL_Rect*, const Position* const);
 
 // adds the position to the SDL_Rect
-void CPT_modifySdlRectPosition(SDL_Rect* rect, const Position pos);
+void CPT_modifySdlRectPosition(SDL_Rect* rect, const Position* const pos);
