758a1d214778ae81fc2b79fe05a5cefc4feef153
jaredponn <jpon1234@gmail.com>
2018-07-08 21:01:56 -0600

    updated to reflect the changes in the engine

--- Src/Main.c
+++ Src/Main.c
@@ -8,8 +8,12 @@ int main(void) {
         struct ECS_ResourceRegistry RESOURCE_REGISTRY;
         ECS_loadInitResources(&RESOURCE_REGISTRY);
 
+        // initalizing the engine
+        struct ECS_Components COMPONENTS;
+        ECS_initComponents(&COMPONENTS, 10000);
+
         // running the game
-        ECS_runEngine(RESOURCE_REGISTRY);
+        ECS_runEngine(&COMPONENTS, &RESOURCE_REGISTRY);
 
         // closing resources
         ECS_destroyRegistry(&RESOURCE_REGISTRY);
