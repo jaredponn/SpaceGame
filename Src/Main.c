5df7edcebe0fb5f26691ad4c5a2dac4bd5d8a63d
jaredponn <jpon1234@gmail.com>
2018-07-16 23:03:58 -0600

    reflect changes to load the engine properly

--- Src/Main.c
+++ Src/Main.c
@@ -4,19 +4,23 @@ int main(void) {
         // initializing the third party libraries (SDL)
         ECS_initLibraries();
 
+        // declaring/ initlizting the core
+        struct ECS_Core engineCore;
+        ECS_initCore(&engineCore);
+
         // loading the resources
-        struct ECS_ResourceRegistry RESOURCE_REGISTRY;
-        ECS_loadInitResources(&RESOURCE_REGISTRY);
+        struct ECS_ResourceRegistry resourceRegistry;
+        ECS_loadInitResources(&resourceRegistry);
 
         // initalizing the engine
-        struct ECS_Components COMPONENTS;
-        ECS_initComponents(&COMPONENTS, 10000);
+        struct ECS_Components engineComponents;
+        ECS_initComponents(&engineComponents, 10000);
 
         // running the game
-        ECS_runEngine(&COMPONENTS, &RESOURCE_REGISTRY);
+        ECS_runEngine(&engineCore, &engineComponents, &resourceRegistry);
 
         // closing resources
-        ECS_destroyRegistry(&RESOURCE_REGISTRY);
+        ECS_destroyRegistry(&resourceRegistry);
         ECS_quitLibraries();
 
         return 0;
