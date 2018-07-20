b937502a859f4fe65d4907bc695df44bbd93106c
jaredponn <jpon1234@gmail.com>
2018-07-20 10:08:19 -0600

    modified to use X macros to generate themanagers

--- Src/Engine/Components/EngineComponents.c
+++ Src/Engine/Components/EngineComponents.c
@@ -1,18 +1,55 @@
 #include "EngineComponents.h"
 #include "stdint.h"
 
+void ECS_initComponents(struct ECS_Components* engineComponents,
+                        size_t capacity) {
+        E_freelist_init(&engineComponents->free_elements);
+        E_freelist_reserve(&engineComponents->free_elements, capacity);
+
+        // initilizes all the components with capacity
+#define X(name) \
+        name##_manager_init(&engineComponents->MANAGER_NAME(name), capacity);
+        LIST_OF_COMPONENTS
+#undef X
+}
+
 size_t ECS_get_next_free_index(struct E_FreeList* freeElements) {
         return E_freelist_add(freeElements, (E){SIZE_MAX});
 }
 
-void ECS_delete_entity_at(struct ECS_Components* components, size_t index) {
+void ECS_delete_entity_at(struct ECS_Components* engineComponents,
+                          size_t index) {
         // marking the index free for the next get_next_free_index function
-        E_freelist_removeat(&components->free_elements, index);
+        E_freelist_removeat(&engineComponents->free_elements, index);
 
-        // deleeting the components each of the managers
-        Entity_manager_remove(&components->entity_manager, index);
-        Acceleration_manager_remove(&components->acceleration_manager, index);
-        Velocity_manager_remove(&components->velocity_manager, index);
-        Position_manager_remove(&components->position_manager, index);
-        Appearance_manager_remove(&components->appearance_manager, index);
+        // deleting the components each of the managers
+#define X(name) \
+        name##_manager_remove(&engineComponents->MANAGER_NAME(name), index);
+        LIST_OF_COMPONENTS
+#undef X
+}
+
+// defining the componenet managers
+#define X(name)                   \
+        VECTOR_DEFINE(name, name) \
+        COMPONENT_MANAGER_DEFINE(name, name)
+LIST_OF_COMPONENTS
+#undef X
+
+// defining the component manager getters
+#define X(name)                                               \
+        struct name##_Manager* MANAGER_GETTER_NAME(name)(     \
+            struct ECS_Components * engineComponents) {       \
+                return &engineComponents->MANAGER_NAME(name); \
+        }
+LIST_OF_COMPONENTS
+#undef X
+
+void ECS_freeComponents(struct ECS_Components* engineComponents) {
+        E_freelist_free(&engineComponents->free_elements);
+
+        // initilizes all the components with capacity
+#define X(name) name##_manager_free(&engineComponents->MANAGER_NAME(name));
+        LIST_OF_COMPONENTS
+#undef X
 }
--- Src/Engine/Components/EngineComponents.h
+++ Src/Engine/Components/EngineComponents.h
@@ -1,12 +1,43 @@
 #pragma once
 
+// lib includes
 #include "Lib/E_FreeList.h"
+#include "Lib/GenericComponentManager.h"
+#include "Lib/GenericVector.h"
 
-#include "Acceleration.h"
 #include "Appearance.h"
 #include "Entity.h"
-#include "Position.h"
-#include "Velocity.h"
+#include "Movement.h"
+
+// -----------------------------------------
+//    Macros
+// -----------------------------------------
+
+// listof components using X macros
+#define LIST_OF_COMPONENTS \
+        X(Entity)          \
+        X(Position)        \
+        X(Velocity)        \
+        X(Acceleration)    \
+        X(Appearance)
+
+// Defines how to name the managers in ECS_Components
+#define MANAGER_NAME(name) m_##name
+
+// defines how to name the manger getters from a ECS_Components
+#define MANAGER_GETTER_NAME(name) ECS_get_##name##_manager
+
+// -----------------------------------------
+//    Declaring the component managers
+// -----------------------------------------
+
+// declares the component managers:
+#define X(name)                    \
+        VECTOR_DECLARE(name, name) \
+        COMPONENT_MANAGER_DECLARE(name, name)
+LIST_OF_COMPONENTS
+#undef X
+
 // -----------------------------------------
 //    Types
 // -----------------------------------------
@@ -18,17 +49,35 @@ struct ECS_Components {
             free_elements;  // for keeping track of where to add and delete
                             // things in the sparse arrays
 
-        // managers
-        struct Entity_Manager entity_manager;
-        struct Acceleration_Manager acceleration_manager;
-        struct Velocity_Manager velocity_manager;
-        struct Position_Manager position_manager;
-        struct Appearance_Manager appearance_manager;
+        // clang-format off
+        // putting the managers inside of this struct
+        #define X(name) struct name##_Manager MANAGER_NAME(name);
+        LIST_OF_COMPONENTS
+        #undef X
+        // clang-format on
 };
 
 // -----------------------------------------
 //    Procedures
 // -----------------------------------------
+
+// initializes the game engine. The size_t initialize size of the vectors
+void ECS_initComponents(struct ECS_Components*, size_t);
+
+// getter for the managers from ECS_Components. returns a non const pointer so
+// permits editing of the manager
+
+/*Example usage: To get the Apearance component
+ * ECS_manager_get(Appearance)(engineComponents_p);
+ */
+#define ECS_manager_get(type) MANAGER_GETTER_NAME(type)
+
+// creating getters for the managers
+#define X(name)                                           \
+        struct name##_Manager* MANAGER_GETTER_NAME(name)( \
+            struct ECS_Components*);
+LIST_OF_COMPONENTS
+#undef X
 
 // gets the next free index and marks that index as taken so that free index
 // will no longer be a free idnex. Basically a wrapper for "freelist_add," and
@@ -37,3 +86,6 @@ size_t ECS_get_next_free_index(struct E_FreeList*);
 
 // deletes an enttiy at a given point
 void ECS_delete_entity_at(struct ECS_Components*, size_t);
+
+// frees the components
+void ECS_freeComponents(struct ECS_Components*);
