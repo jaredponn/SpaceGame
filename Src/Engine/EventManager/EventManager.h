59fc1b8f560f724cb2d359cf1ca4ffc2585670f7
jaredponn <jpon1234@gmail.com>
2018-07-21 10:50:41 -0600

    added comments

--- Src/Engine/EventManager/EventManager.h
+++ Src/Engine/EventManager/EventManager.h
@@ -2,6 +2,11 @@
 
 #include "Lib/GenericVector.h"
 #include "Src/Engine/Components/Entity.h"
+/**
+ * Event manager ----
+ * The eventmanager is a vector of Events. The Event is a wrapper around a type
+ * and the Signal (each input event can be thought of as a signal from a system)
+ */
 
 // -----------------------------------------
 //    Primitive event types
@@ -9,29 +14,29 @@
 
 // enum to keep track of the different types an event could be
 typedef enum EVT_Type {
-        EVT_Collision,
+        EVT_Collision, /**< basic collision signal */
 } EVT_Type;
 
 // the global_index of the entities that have collided
-typedef struct EVT_CollisionEvent {
+typedef struct EVT_CollisionSignal {
         size_t a; /**< id of the first entity that collided */
         size_t b; /**< id of the second entity */
-} EVT_CollisionEvent;
+} EVT_CollisionSignal;
 
 // -----------------------------------------
 //    Higher order event types
 // -----------------------------------------
 
 // union that contains the data
-typedef union EVT_EventData {
-        EVT_CollisionEvent collision;
+typedef union EVT_Signal {
+        EVT_CollisionSignal collision;
 
 } EVT_Event;
 
 // Event type
 typedef struct Event {
-        unsigned int type;         /**< type that each event should have */
-        union EVT_EventData event; /**< data for the event */
+        unsigned int type;      /**< type that each event should have */
+        union EVT_Signal event; /**< data for the event */
 } Event;
 
 VECTOR_DECLARE(Event, Event)
