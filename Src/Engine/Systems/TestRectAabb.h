5cd2d2738b8f266783751a8700241b80ece8e883
jaredponn <jpon1234@gmail.com>
2018-07-21 00:12:55 -0600

    shell of how the system will test collisions

---
+++ Src/Engine/Systems/TestRectAabb.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// -----------------------------------------
+//    Tests if the rectangles from the component mangers are colliding. if they
+//    are, it will
+// -----------------------------------------
