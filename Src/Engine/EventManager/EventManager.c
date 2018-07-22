2ced87b3f0ab131c22ad962abc6a5b4a52ec0844
jaredponn <jpon1234@gmail.com>
2018-07-22 00:51:38 -0600

    modified event manager to what it really is - just a vector

--- Src/Engine/EventManager/EventManager.c
+++ Src/Engine/EventManager/EventManager.c
@@ -1,22 +1,3 @@
 #include "EventManager.h"
 
-void EVT_push_back(struct ECS_EventManager* eventManager, const Event* event) {
-        Event_vector_push_back(&eventManager->events, *event);
-}
-
-const struct Event_Vector* EVT_get_events(
-    const struct ECS_EventManager* eventManager) {
-        return &eventManager->events;
-}
-
-void EVT_clear(struct ECS_EventManager* eventManager) {
-        Event_vector_lazy_clear(&eventManager->events);
-}
-
-void ECS_initEventManager(struct ECS_EventManager* eventManager,
-                          size_t capacity) {
-        Event_vector_init(&eventManager->events);
-        Event_vector_reserve(&eventManager->events, capacity);
-}
-
 VECTOR_DEFINE(Event, Event)
--- Src/Engine/EventManager/EventManager.h
+++ Src/Engine/EventManager/EventManager.h
@@ -14,6 +14,8 @@
 
 // enum to keep track of the different types an event could be
 typedef enum EVT_Type {
+        EVT_Empty,     /**< no event*/
+        EVT_Spawn,     /**< basic spawn signal */
         EVT_Collision, /**< basic collision signal */
 } EVT_Type;
 
@@ -30,33 +32,12 @@ typedef struct EVT_CollisionSignal {
 // union that contains the data
 typedef union EVT_Signal {
         EVT_CollisionSignal collision;
-
 } EVT_Event;
 
 // Event type
 typedef struct Event {
-        unsigned int type;      /**< type that each event should have */
-        union EVT_Signal event; /**< data for the event */
+        unsigned int type;       /**< type that each event should have */
+        union EVT_Signal signal; /**< data for the event */
 } Event;
 
 VECTOR_DECLARE(Event, Event)
-
-struct ECS_EventManager {
-        struct Event_Vector events; /**< list of events */
-};
-
-// -----------------------------------------
-//    Procedures
-// -----------------------------------------
-
-// pushes an event
-void EVT_push_back(struct ECS_EventManager*, const Event*);
-
-// gets a const pointer to vector
-const struct Event_Vector* EVT_get_events(const struct ECS_EventManager*);
-
-// lazily clears the events in the event manager
-void EVT_clear(struct ECS_EventManager*);
-
-// initlizes the event manager with a given size_t
-void ECS_initEventManager(struct ECS_EventManager*, size_t);
