8ceae97e1eeedbaa9a393e0d3751ac3e2ad38e95
jaredponn <jpon1234@gmail.com>
2018-07-22 00:52:14 -0600

    modified the input so that we have one handler and a struct of key mappingsto decide what events ot fire off to the event system

--- Src/Engine/Input/InputHandler.c
+++ Src/Engine/Input/InputHandler.c
@@ -1,1 +1,163 @@
 #include "InputHandler.h"
+
+// static read only global variable of the mouse's position and scroll direction
+static struct INP_MouseState globalMouseState;
+
+// -----------------------------------------
+//    Procedures
+// -----------------------------------------
+
+void ECS_InputMap_init(struct INP_InputMap* inputMap) {
+        globalMouseState =
+            (struct INP_MouseState){.mouse_position = (struct V2){0, 0},
+                                    .scroll_direction = (struct V2){0, 0}};
+
+        // initlizing the vectors
+        KeyBind_vector_init(&inputMap->keyUpMappings);
+        KeyBind_vector_init(&inputMap->keyDownMappings);
+
+        MouseKeyBind_vector_init(&inputMap->mouseButtonUpMappings);
+        MouseKeyBind_vector_init(&inputMap->mouseButtonDownMappings);
+
+        inputMap->mouseMovementEvent = (Event){.type = EVT_Empty};
+        inputMap->mouseScrollEvent = (Event){.type = EVT_Empty};
+}
+
+void INP_clearInputMap(struct INP_InputMap* inputMap) {
+        // initlizing the vectors
+        KeyBind_vector_lazy_clear(&inputMap->keyUpMappings);
+        KeyBind_vector_lazy_clear(&inputMap->keyDownMappings);
+
+        MouseKeyBind_vector_lazy_clear(&inputMap->mouseButtonUpMappings);
+        MouseKeyBind_vector_lazy_clear(&inputMap->mouseButtonDownMappings);
+}
+
+void INP_parseInputs(SDL_Event* sdlEvent, struct INP_InputMap* inputMap,
+                     struct Event_Vector* eventManager) {
+        size_t keyDownVecLength =
+            KeyBind_vector_size(&inputMap->keyDownMappings);
+        size_t keyUpVecLength = KeyBind_vector_size(&inputMap->keyUpMappings);
+
+        size_t mouseKeyDownVecLength =
+            MouseKeyBind_vector_size(&inputMap->mouseButtonDownMappings);
+        size_t mouseKeyUpVecLength =
+            MouseKeyBind_vector_size(&inputMap->mouseButtonDownMappings);
+
+        while (SDL_PollEvent(sdlEvent)) {
+                // changes the button states (mouse and keyboard)
+                switch (sdlEvent->type) {
+                        case SDL_KEYDOWN:
+                                for (size_t i = 0; i < keyDownVecLength; ++i) {
+                                        if (sdlEvent->key.keysym.scancode ==
+                                            KeyBind_vector_get(
+                                                &inputMap->keyDownMappings, i)
+                                                .sdlKey)
+                                                Event_vector_push_back(
+                                                    eventManager,
+                                                    KeyBind_vector_get(
+                                                        &inputMap
+                                                             ->keyDownMappings,
+                                                        i)
+                                                        .gameEvent);
+                                }
+                                break;
+
+                        case SDL_KEYUP:
+                                for (size_t i = 0; i < keyUpVecLength; ++i) {
+                                        if (sdlEvent->key.keysym.scancode ==
+                                            KeyBind_vector_get(
+                                                &inputMap->keyUpMappings, i)
+                                                .sdlKey)
+                                                Event_vector_push_back(
+                                                    eventManager,
+                                                    KeyBind_vector_get(
+                                                        &inputMap
+                                                             ->keyUpMappings,
+                                                        i)
+                                                        .gameEvent);
+                                }
+                                break;
+
+                        case SDL_MOUSEBUTTONDOWN:
+                                for (size_t i = 0; i < mouseKeyDownVecLength;
+                                     ++i) {
+                                        if (sdlEvent->button.button ==
+                                            MouseKeyBind_vector_get(
+                                                &inputMap
+                                                     ->mouseButtonDownMappings,
+                                                i)
+                                                .button)
+                                                Event_vector_push_back(
+                                                    eventManager,
+                                                    MouseKeyBind_vector_get(
+                                                        &inputMap
+                                                             ->mouseButtonDownMappings,
+                                                        i)
+                                                        .gameEvent);
+                                }
+                                break;
+
+                        case SDL_MOUSEBUTTONUP:
+                                for (size_t i = 0; i < mouseKeyUpVecLength;
+                                     ++i) {
+                                        if (sdlEvent->button.button ==
+                                            MouseKeyBind_vector_get(
+                                                &inputMap
+                                                     ->mouseButtonUpMappings,
+                                                i)
+                                                .button)
+                                                Event_vector_push_back(
+                                                    eventManager,
+                                                    MouseKeyBind_vector_get(
+                                                        &inputMap
+                                                             ->mouseButtonUpMappings,
+                                                        i)
+                                                        .gameEvent);
+                                }
+                                break;
+
+                        case SDL_MOUSEWHEEL:
+                                globalMouseState.scroll_direction =
+                                    (struct V2){.x = (float)sdlEvent->wheel.x,
+                                                .y = (float)sdlEvent->wheel.y};
+
+                                Event_vector_push_back(
+                                    eventManager, inputMap->mouseScrollEvent);
+                                break;
+
+                        case SDL_MOUSEMOTION:
+                                globalMouseState.scroll_direction =
+                                    (struct V2){.x = (float)sdlEvent->button.x,
+                                                .y = (float)sdlEvent->button.y};
+                                Event_vector_push_back(
+                                    eventManager, inputMap->mouseMovementEvent);
+                                break;
+                        case SDL_QUIT: {
+                                exit(1);
+                        }
+
+                        default:
+                                break;
+                }
+        }
+}
+
+const struct V2* INP_getMousePosition() {
+        return &globalMouseState.mouse_position;
+}
+
+const struct V2* INP_getScroll() { return &globalMouseState.scroll_direction; }
+
+void ECS_free_InputMap(struct INP_InputMap* inputMap) {
+        KeyBind_vector_free(&inputMap->keyUpMappings);
+        KeyBind_vector_free(&inputMap->keyDownMappings);
+
+        MouseKeyBind_vector_free(&inputMap->mouseButtonUpMappings);
+        MouseKeyBind_vector_free(&inputMap->mouseButtonDownMappings);
+}
+// -----------------------------------------
+//    Macro definitions
+// -----------------------------------------
+
+VECTOR_DEFINE(struct INP_KeyBind, KeyBind)
+VECTOR_DEFINE(struct INP_MouseKeyBind, MouseKeyBind)
--- Src/Engine/Input/InputHandler.h
+++ Src/Engine/Input/InputHandler.h
@@ -1,5 +1,99 @@
 #pragma once
+#include <SDL2/SDL.h>
+#include "../EventManager/EventManager.h"
+#include "Lib/GenericVector.h"
+#include "Lib/V2.h"
 
 /**
  * Input handler
  */
+
+// -----------------------------------------
+//    Raw events to game event bindings
+// -----------------------------------------
+
+// bindings from an SDL_Key keyboard event to a game event
+struct INP_KeyBind;
+struct INP_KeyBind {
+        Uint32 sdlKey;   /**< the key event in SDL:
+                            https://wiki.libsdl.org/SDL_Scancode  */
+        Event gameEvent; /**< the game event that corrosponds to the SDL event*/
+};
+VECTOR_DECLARE(struct INP_KeyBind, KeyBind)
+
+// bindings from a SDL mouse button event
+struct INP_MouseKeyBind;
+struct INP_MouseKeyBind {
+        Uint8 button;    /**< the SDL mousebutton that was pressed:
+                            SDL_BUTTON_LEFT, SDL_BUTTON_MIDDLE, SDL_BUTTON_RIGHT,
+                            SDL_BUTTONX1, SDL_BUTTON_X2 */
+        Event gameEvent; /**< the game event that corrosponds to the SDL event*/
+};
+VECTOR_DECLARE(struct INP_MouseKeyBind, MouseKeyBind)
+
+// game event to execute when the mouse is moved
+// struct INP_MouseMovement;
+// struct INP_MouseMovement {
+//         Event gameEvent; [>*< the game event <]
+// };
+typedef Event INP_MouseMovement;
+
+// game event to execute when the mouse wheel is moved
+// struct INP_MouseScroll;
+// struct INP_MouseScroll {
+//         Event gameEvent; [>*< the game event <]
+// };
+typedef Event INP_MouseScroll;
+
+// -----------------------------------------
+//    Input map
+// -----------------------------------------
+struct INP_InputMap;
+struct INP_InputMap {
+        struct KeyBind_Vector keyUpMappings;   /**< vector of the bindings given
+                                                  in a key up event */
+        struct KeyBind_Vector keyDownMappings; /**< vector of the bindings given
+                                                in a keydown event */
+        struct MouseKeyBind_Vector
+            mouseButtonUpMappings; /**< vector of bindings when the mouse button
+                                      is released (SDL_RELEASED) */
+        struct MouseKeyBind_Vector
+            mouseButtonDownMappings; /**< vector of bindings when the mouse
+                                      button is released (SDL_PRESSED)*/
+
+        INP_MouseMovement mouseMovementEvent; /**< on mouse event gameevent  */
+
+        INP_MouseScroll
+            mouseScrollEvent; /**< on mouse scroll event gameevent */
+};
+
+// -----------------------------------------
+//    Global mouse state type (should be read only)
+// -----------------------------------------
+struct INP_MouseState;
+struct INP_MouseState {
+        struct V2 mouse_position;
+        struct V2 scroll_direction;
+};
+
+// -----------------------------------------
+//    Procedures
+// -----------------------------------------
+
+// initlizes the input map, and the global state variables for the mouse
+void ECS_InputMap_init(struct INP_InputMap*);
+
+// clears the input map
+void INP_clearInputMap(struct INP_InputMap*);
+
+// parses the SDL_Event's and sends them to the Event_Vector
+void INP_parseInputs(SDL_Event*, struct INP_InputMap*, struct Event_Vector*);
+
+// getting the mouse position
+const struct V2* INP_getMousePosition();
+
+// get scroll direction
+const struct V2* INP_getScroll();
+
+// initlizes the input map, and the global state variables for the mouse
+void ECS_free_InputMap(struct INP_InputMap*);
