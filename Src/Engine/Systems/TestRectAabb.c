f7ec26487fb557f936601a4e030207df1a2e9187
jaredponn <jpon1234@gmail.com>
2018-07-23 18:42:11 -0600

    refactoring

--- Src/Engine/Systems/TestRectAabb.c
+++
@@ -1,1 +0,0 @@
-#include "TestRectAabb.h"
--- Src/Engine/Systems/TestRectAabb.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// -----------------------------------------
-//    Tests if the rectangles from the component mangers are colliding. if they
-//    are, it will
-// -----------------------------------------
