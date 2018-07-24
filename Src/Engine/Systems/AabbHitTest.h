b5498a01edf42c8ec6d85a991fca82b52ef000e4
jaredponn <jpon1234@gmail.com>
2018-07-23 18:41:38 -0600

    beginning creating the hit test system

---
+++ Src/Engine/Systems/AabbHitTest.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Src/Engine/Components/EngineComponents.h"
+#include "Src/Engine/EventManager/EventManager.h"
+
+// -----------------------------------------
+//    Tests if the rectangles from the component mangers are colliding. if they
+//    are, it will send the event to the event manager
+// -----------------------------------------
+
+void SYS_rectAabbHitTest();
