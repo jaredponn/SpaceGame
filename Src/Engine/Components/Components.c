1fa4bc8689061ca735032a72e1d781d2019a3c1b
jaredponn <jpon1234@gmail.com>
2018-07-27 21:37:58 -0600

    updated the delete function, and add function so they play nicely with the rest of the system

--- Src/Engine/Components/Components.c
+++ Src/Engine/Components/Components.c
@@ -1,81 +1,111 @@
 #include "Components.h"
 #include "stdint.h"
 
-void CPT_initComponents(struct CPT_Components* engineComponents,
-                        size_t capacity) {
-        EFreeList_init(&engineComponents->free_elements);
-        EFreeList_reserve(&engineComponents->free_elements, capacity);
+void CPT_initComponents(struct CPT_Components *engineComponents,
+			size_t capacity)
+{
+	MarkedFreeList_init(&engineComponents->free_elements);
+	MarkedFreeList_reserve(&engineComponents->free_elements, capacity);
 
-        // initilizes all the components with capacity
-#define X_CPT(type, name) \
-        name##Manager_init(&engineComponents->MANAGER_NAME(name), capacity);
-        LIST_OF_COMPONENTS
+	// initilizes all the components with capacity
+#define X_CPT(type, name)                                                      \
+	name##Manager_init(&engineComponents->MANAGER_NAME(name), capacity);
+	LIST_OF_COMPONENTS
 #undef X_CPT
 }
 
-size_t CPT_updateCurFreeIndex(struct CPT_Components* components) {
-        return EFreeList_add(&components->free_elements, (E){SIZE_MAX});
+// TODO broken
+void CPT_updateCurFreeIndex(struct CPT_Components *components)
+{
+	struct MarkedFreeList *freeElements = &components->free_elements;
+	if (MarkedFreeList_is_index_used(freeElements,
+					 CPT_getCurFreeIndex(components))) {
+		/** (void)MarkedFreeList_add(&components->free_elements, */
+		/**                          (MarkedIndex){true, SIZE_MAX}); */
+		MarkedFreeList_update_index(freeElements);
+	}
 }
 
-size_t CPT_getCurFreeIndex(struct CPT_Components* components) {
-        return EFreeList_get_curr_free_index(&components->free_elements);
+size_t CPT_getCurFreeIndex(struct CPT_Components *components)
+{
+
+	return MarkedFreeList_get_curr_free_index(&components->free_elements);
 }
 
-void CPT_deleteEntityAt(struct CPT_Components* engineComponents, size_t index) {
-        // marking the index free for the next get_next_free_index function
-        EFreeList_remove_at(&engineComponents->free_elements, index);
+void CPT_deleteEntityAt(struct CPT_Components *engineComponents, size_t index)
+{
+	// updates the next free index so the free list updates properly
+	CPT_updateCurFreeIndex(engineComponents);
 
-        // deleting the components each of the managers
-#define X_CPT(type, name) \
-        name##Manager_remove_at(&engineComponents->MANAGER_NAME(name), index);
-        LIST_OF_COMPONENTS
+	// marking the index free for the next get_next_free_index function
+	struct MarkedFreeList *freeElements = &engineComponents->free_elements;
+	if (MarkedIndexVector_size(&freeElements->data) > index
+	    && MarkedFreeList_is_index_used(
+		       freeElements,
+		       index)) // checks to see if there is something first
+	{
+		MarkedFreeList_remove_at(freeElements, index);
+		MarkedFreeList_mark_index_as_free(freeElements, index);
+
+		// deleting the components each of the managers
+#define X_CPT(type, name)                                                      \
+	name##Manager_remove_at(&engineComponents->MANAGER_NAME(name), index);
+		LIST_OF_COMPONENTS
 #undef X_CPT
+	}
 }
 
 // defining the componenet managers
-#define X_CPT(type, name)         \
-        VECTOR_DEFINE(type, name) \
-        COMPONENT_MANAGER_DEFINE(type, name, name)
+#define X_CPT(type, name)                                                      \
+	VECTOR_DEFINE(type, name)                                              \
+	COMPONENT_MANAGER_DEFINE(type, name, name)
 LIST_OF_COMPONENTS
 #undef X_CPT
 
 // defining the component manager getters
-#define X_CPT(type, name)                                     \
-        struct name##Manager* MANAGER_GETTER_NAME(name)(      \
-            struct CPT_Components * engineComponents) {       \
-                return &engineComponents->MANAGER_NAME(name); \
-        }
+#define X_CPT(type, name)                                                      \
+	struct name##Manager *MANAGER_GETTER_NAME(name)(struct CPT_Components  \
+							* engineComponents)    \
+	{                                                                      \
+		return &engineComponents->MANAGER_NAME(name);                  \
+	}
 LIST_OF_COMPONENTS
 #undef X_CPT
 
 // CPT_add<name>At(components, index, val )
-#define X_CPT(type, name)                                                     \
-        void MANAGER_ADD_AT_NAME(name)(                                       \
-            struct CPT_Components * engineComponents, size_t index,           \
-            type * val) {                                                     \
-                name##Manager_add_at(                                         \
-                    MANAGER_GETTER_NAME(name)(engineComponents), index, val); \
-        }
+#define X_CPT(type, name)                                                      \
+	void MANAGER_ADD_AT_NAME(name)(struct CPT_Components                   \
+					       * engineComponents,             \
+				       size_t index, type * val)               \
+	{                                                                      \
+		MarkedFreeList_mark_index_as_used(                             \
+			&engineComponents->free_elements, index);              \
+		name##Manager_add_at(                                          \
+			MANAGER_GETTER_NAME(name)(engineComponents), index,    \
+			val);                                                  \
+	}
 LIST_OF_COMPONENTS
 #undef X_CPT
 
 // CPT_add<name>(components, index, val )
-#define X_CPT(type, name)                                               \
-        void MANAGER_ADD_NAME(name)(struct CPT_Components * components, \
-                                    type * val) {                       \
-                MANAGER_ADD_AT_NAME(name)                               \
-                (components, CPT_getCurFreeIndex(components), val);     \
-        }
+#define X_CPT(type, name)                                                      \
+	void MANAGER_ADD_NAME(name)(struct CPT_Components * components,        \
+				    type * val)                                \
+	{                                                                      \
+		MANAGER_ADD_AT_NAME(name)                                      \
+		(components, CPT_getCurFreeIndex(components), val);            \
+	}
 LIST_OF_COMPONENTS
 #undef X_CPT
 
 // free components
-void CPT_freeComponents(struct CPT_Components* engineComponents) {
-        EFreeList_free(&engineComponents->free_elements);
+void CPT_freeComponents(struct CPT_Components *engineComponents)
+{
+	MarkedFreeList_free(&engineComponents->free_elements);
 
-        // initilizes all the components with capacity
-#define X_CPT(type, name) \
-        name##Manager_free(&engineComponents->MANAGER_NAME(name));
-        LIST_OF_COMPONENTS
+	// initilizes all the components with capacity
+#define X_CPT(type, name)                                                      \
+	name##Manager_free(&engineComponents->MANAGER_NAME(name));
+	LIST_OF_COMPONENTS
 #undef X_CPT
 }
--- Src/Engine/Components/Components.h
+++ Src/Engine/Components/Components.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Lib/E_FreeList.h"
+#include "Lib/MarkedFreeList.h"
 #include "Lib/GenericComponentManager.h"
 #include "Lib/GenericVector.h"
 #include "Lib/Macros.h"
@@ -21,14 +21,15 @@
 
 // listof components using X macros. Used to generate vectors, and packed
 // vectors of the given type, prefixed by the type.
-#define LIST_OF_COMPONENTS                 \
-        /*X_CPT(TYPE, VEC/PKDVEC PREFIX)*/ \
-        X_CPT(Entity, Entity)              \
-        X_CPT(Position, Position)          \
-        X_CPT(Velocity, Velocity)          \
-        X_CPT(Acceleration, Acceleration)  \
-        X_CPT(Appearance, Appearance)      \
-        X_CPT(Aabb, Aabb)
+#define LIST_OF_COMPONENTS                                                     \
+	/*X_CPT(TYPE, VEC/PKDVEC PREFIX)*/                                     \
+	X_CPT(Entity, Entity)                                                  \
+	X_CPT(Position, Position)                                              \
+	X_CPT(Velocity, Velocity)                                              \
+	X_CPT(Acceleration, Acceleration)                                      \
+	X_CPT(Appearance, Appearance)                                          \
+	X_CPT(ARectAabb, ARectAabb)                                            \
+	X_CPT(BRectAabb, BRectAabb)
 
 // Defines how to name the managers in CPT_Components
 #define MANAGER_NAME(name) m_##name
@@ -46,9 +47,9 @@
 // -----------------------------------------
 
 // declares the component managers:
-#define X_CPT(type, name)          \
-        VECTOR_DECLARE(type, name) \
-        COMPONENT_MANAGER_DECLARE(type, name, name)
+#define X_CPT(type, name)                                                      \
+	VECTOR_DECLARE(type, name)                                             \
+	COMPONENT_MANAGER_DECLARE(type, name, name)
 LIST_OF_COMPONENTS
 #undef X_CPT
 
@@ -59,15 +60,15 @@ LIST_OF_COMPONENTS
 // god object of state
 struct CPT_Components;
 struct CPT_Components {
-        struct EFreeList free_elements; /**< keepstrack of where to add and
-                                           delete things in the sparse arrays */
+	struct MarkedFreeList free_elements; /**< keepstrack of where to add and
+					   delete things in the sparse arrays */
 
 // clang-format off
         // putting the managers inside of this struct
         #define X_CPT(type, name) struct name##Manager MANAGER_NAME(name);
         LIST_OF_COMPONENTS
         #undef X_CPT
-        // clang-format on
+	// clang-format on
 };
 
 // -----------------------------------------
@@ -75,21 +76,21 @@ struct CPT_Components {
 // -----------------------------------------
 
 // initializes the compoenets engine. The size_t initialize size of the vectors
-void CPT_initComponents(struct CPT_Components*, size_t);
+void CPT_initComponents(struct CPT_Components *, size_t);
 
 // updates the next free index, so when calling CPT_getCurFreeIndex, it will
 // give a free index. Basically a wrapper for
 // "freelist_add," and fills the added element with SIZE_MAX
-size_t CPT_updateCurFreeIndex(struct CPT_Components*);
+void CPT_updateCurFreeIndex(struct CPT_Components *);
 
 // gets the current free index
-size_t CPT_getCurFreeIndex(struct CPT_Components*);
+size_t CPT_getCurFreeIndex(struct CPT_Components *);
 
 // deletes an enttiy at a given point
-void CPT_deleteEntityAt(struct CPT_Components*, size_t);
+void CPT_deleteEntityAt(struct CPT_Components *, size_t);
 
 // frees the components
-void CPT_freeComponents(struct CPT_Components*);
+void CPT_freeComponents(struct CPT_Components *);
 
 // -----------------------------------------
 //    Getters
@@ -103,8 +104,9 @@ void CPT_freeComponents(struct CPT_Components*);
 #define CPT_managerGet(name) MANAGER_GETTER_NAME(name)
 
 /// CPT_get<name>manager
-#define X_CPT(type, name) \
-        struct name##Manager* MANAGER_GETTER_NAME(name)(struct CPT_Components*);
+#define X_CPT(type, name)                                                      \
+	struct name##Manager *MANAGER_GETTER_NAME(name)(                       \
+		struct CPT_Components *);
 LIST_OF_COMPONENTS
 #undef X_CPT
 
@@ -114,13 +116,13 @@ LIST_OF_COMPONENTS
 // strictly internals are here for the EngineComponentGenerics.h file
 
 // CPT_add<name>At(components, index, val )
-#define X_CPT(type, name) \
-        void MANAGER_ADD_AT_NAME(name)(struct CPT_Components*, size_t, type*);
+#define X_CPT(type, name)                                                      \
+	void MANAGER_ADD_AT_NAME(name)(struct CPT_Components *, size_t, type *);
 LIST_OF_COMPONENTS
 #undef X_CPT
 
 // CPT_add<name>(components, index, val )
-#define X_CPT(type, name) \
-        void MANAGER_ADD_NAME(name)(struct CPT_Components*, type*);
+#define X_CPT(type, name)                                                      \
+	void MANAGER_ADD_NAME(name)(struct CPT_Components *, type *);
 LIST_OF_COMPONENTS
 #undef X_CPT
