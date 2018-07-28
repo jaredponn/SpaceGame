a2f4eef50c286c7721921a184ac2123c9024c3ba
jaredponn <jpon1234@gmail.com>
2018-07-27 21:33:39 -0600

    added functionality that uses a bitshifted int to check if the index is marked

--- Lib/E_FreeList.c
+++ Lib/E_FreeList.c
@@ -2,3 +2,25 @@
 
 VECTOR_DEFINE(E, E)
 FREELIST_DEFINE(E, E, E)
+
+// 1 denotes is free
+size_t isFree(E *n)
+{
+	return n->next_free_index & 1;
+}
+
+// 1 denotes is free
+void invertIsFree(E *n)
+{
+	n->next_free_index = n->next_free_index ^ 1;
+}
+
+size_t getIndex(E *n)
+{
+	return n->next_free_index >> 1;
+}
+
+void setIndex(E *n, size_t val)
+{
+	n->next_free_index = (val << 1) | isFree(n);
+}
--- Lib/E_FreeList.h
+++ Lib/E_FreeList.h
@@ -11,10 +11,30 @@
 
 struct FL_index;
 struct FL_index {
-        size_t next_free_index;
+	size_t next_free_index;
 };
 
 typedef struct FL_index E;
 
-VECTOR_DECLARE(E, E)  // declares vector of type E -> EVector
+VECTOR_DECLARE(E, E) // declares vector of type E -> EVector
 FREELIST_DECLARE(E, E, E)
+
+
+// -----------------------------------------
+//    Extra functionality to work with the E data type.
+// -----------------------------------------
+/** These functions can be used to mark whether that specific index is "free" or
+ * "used", while keepig track of the next free index.
+ */
+
+// returns whether it is free or not
+size_t isFree(E *n);
+
+// inverses whether it is free or not (NOT)
+void invertIsFree(E *n);
+
+// gets the index
+size_t getIndex(E *n);
+
+// sets the index
+void setIndex(E *n, size_t val);
