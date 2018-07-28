83ad408e574702d601a2f4009063c8ed23bdb67f
jaredponn <jpon1234@gmail.com>
2018-07-27 21:39:19 -0600

    added basic collision signals

--- Src/Engine/EventManager/EventManager.h
+++ Src/Engine/EventManager/EventManager.h
@@ -14,30 +14,31 @@
 
 // enum to keep track of the different types an event could be
 typedef enum EVT_Type {
-        EVT_Empty,     /**< no event*/
-        EVT_Spawn,     /**< basic spawn signal */
-        EVT_Collision, /**< basic collision signal */
+	EVT_Empty,     /**< no event*/
+	EVT_SpawnA,    /**< basic spawn signal */
+	EVT_SpawnB,    /**< basic spawn signal */
+	EVT_Collision, /**< basic collision signal */
 } EVT_Type;
 
 // the global_index of the entities that have collided
 typedef struct EVT_CollisionSignal {
-        size_t a; /**< id of the first entity that collided */
-        size_t b; /**< id of the second entity */
+	size_t a; /**< id of the first entity that collided */
+	size_t b; /**< id of the second entity */
 } EVT_CollisionSignal;
 
 // -----------------------------------------
 //    Higher order event types
 // -----------------------------------------
 
-// union that contains the data
-typedef union EVT_Signal {
-        EVT_CollisionSignal collision;
-} EVT_Signal;
-
 // Event type
 typedef struct Event {
-        EVT_Type type;           /**< type that each event should have */
-        union EVT_Signal signal; /**< data for the event */
+	EVT_Type type; /**< type that each event should have */
+
+	/** signals of possible events */
+	union {
+		EVT_CollisionSignal collision;
+	};
+
 } Event;
 
 // the event manager is a wrapper around a vector object
