75a0da5f7fc7d58d842dfb5f662a1128d4c3e4e4
jaredponn <jpon1234@gmail.com>
2018-07-27 21:39:30 -0600

    added a button to spawn another rectangle

--- Src/Engine/Input/GameInputMaps.c
+++ Src/Engine/Input/GameInputMaps.c
@@ -1,11 +1,17 @@
 #include "GameInputMaps.h"
 #include "../EventManager/EventManager.h"
 
-void INP_setDefaultMap(struct INP_InputMap* inputMap) {
-        INP_clearInputMap(inputMap);
+void INP_setDefaultMap(struct INP_InputMap *inputMap)
+{
+	INP_clearInputMap(inputMap);
 
-        INP_addKeyReleaseBinding(
-            inputMap,
-            ((struct INP_KeyBind){.sdlKey = SDL_SCANCODE_A,
-                                  .gameEvent = (Event){.type = EVT_Spawn}}));
+	INP_addKeyReleaseBinding(
+		inputMap, ((struct INP_KeyBind){
+				  .sdlKey = SDL_SCANCODE_A,
+				  .gameEvent = (Event){.type = EVT_SpawnA}}));
+
+	INP_addKeyReleaseBinding(
+		inputMap, ((struct INP_KeyBind){
+				  .sdlKey = SDL_SCANCODE_B,
+				  .gameEvent = (Event){.type = EVT_SpawnB}}));
 }
