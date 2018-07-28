70fe9ff58846a43d3fd46573403d48ed8512c096
jaredponn <jpon1234@gmail.com>
2018-07-27 21:36:48 -0600

    updated to reflect unit test changes'

--- UnitTesting/Run_Tests.c
+++ UnitTesting/Run_Tests.c
@@ -3,9 +3,9 @@
 
 // Component test includes
 #include "Src/tests/CPT_Appearance_Tests.h"
+#include "Src/tests/CPT_Components_Tests.h"
 
 // Lib tests includes
-//#include "Lib/tests/ComponentManager_Tests.h"
 #include "Lib/tests/Freelist_Test.h"
 #include "Lib/tests/PackedVector_Tests.h"
 #include "Lib/tests/Stack_Tests.h"
@@ -13,70 +13,70 @@
 #include "Lib/tests/Vector_Tests.h"
 
 // -----------------------------------------
-//    Function declarations
-// -----------------------------------------
-
-// generic run test function
-int runTest(int (*fp)(Suite*, SRunner*));
-
-// lib test suite
-int libTests(Suite* s, SRunner* sr);
-
-// cpt test suite
-
-// -----------------------------------------
 //    Generic run test function
 // -----------------------------------------
 
-int runTest(int (*fp)(Suite*, SRunner*)) {
-        int number_failed = 0;
-        Suite* s = NULL;
-        SRunner* sr = NULL;
+int runTest(int (*fp)(Suite *, SRunner *))
+{
+	int number_failed = 0;
+	Suite *s = NULL;
+	SRunner *sr = NULL;
 
-        number_failed = (*fp)(s, sr);
+	number_failed = (*fp)(s, sr);
 
-        srunner_free(sr);
+	srunner_free(sr);
 
-        return number_failed;
+	return number_failed;
 }
 
 // -----------------------------------------
 //    Test suites
 // -----------------------------------------
+int cptEngineTests(Suite *s, SRunner *sr)
+{
+	s = cpt_engine_test_suite();
+	sr = srunner_create(s);
 
-int cptTests(Suite* s, SRunner* sr) {
-        s = appearances_test_suite();
-        sr = srunner_create(s);
-
-        srunner_run_all(sr, CK_NORMAL);
-        return srunner_ntests_failed(sr);
+	srunner_run_all(sr, CK_NORMAL);
+	return srunner_ntests_failed(sr);
 }
 
-int libTests(Suite* s, SRunner* sr) {
-        // starts withe the vector test suite
-        s = vector_test_suite();
-        sr = srunner_create(s);
+int specificCptTests(Suite *s, SRunner *sr)
+{
+	s = appearances_test_suite();
+	sr = srunner_create(s);
 
-        // adds the rest of the test suites
-        srunner_add_suite(sr, stack_test_suite());
-        srunner_add_suite(sr, freelist_test_suite());
-        srunner_add_suite(sr, UTI_strcatTestSuite());
-        srunner_add_suite(sr, packedvector_test_suite());
-        // srunner_add_suite(sr, componentmanager_test_suite());
+	srunner_run_all(sr, CK_NORMAL);
+	return srunner_ntests_failed(sr);
+}
 
-        srunner_run_all(sr, CK_NORMAL);
+int libTests(Suite *s, SRunner *sr)
+{
+	// starts withe the vector test suite
+	s = vector_test_suite();
+	sr = srunner_create(s);
 
-        return srunner_ntests_failed(sr);
+	// adds the rest of the test suites
+	srunner_add_suite(sr, stack_test_suite());
+	srunner_add_suite(sr, freelist_test_suite());
+	srunner_add_suite(sr, UTI_strcatTestSuite());
+	srunner_add_suite(sr, packedvector_test_suite());
+
+	srunner_run_all(sr, CK_NORMAL);
+
+	return srunner_ntests_failed(sr);
 }
 // -----------------------------------------
 //    Main
 // -----------------------------------------
 
-int main(void) {
-        int number_failed = 0;
+int main(void)
+{
+	int number_failed = 0;
 
-        number_failed += runTest(&libTests);
-        number_failed += runTest(&cptTests);
+	number_failed += runTest(&libTests);
+	number_failed += runTest(&cptEngineTests);
+	number_failed += runTest(&specificCptTests);
 
-        return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
