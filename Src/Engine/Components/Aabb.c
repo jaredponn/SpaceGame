972edd3d33fcbb83b1f1587443ab63630e3eeda8
jaredponn <jpon1234@gmail.com>
2018-07-27 21:37:36 -0600

    changed to go back to th emacro hackery

--- Src/Engine/Components/Aabb.c
+++ Src/Engine/Components/Aabb.c
@@ -11,28 +11,43 @@
 //    function implementations
 // -----------------------------------------
 
-bool CPT_hitTestRectAabb(const RectAabb* a, const RectAabb* b) {
-        return (a->pMin.x <= b->pMax.x && a->pMax.x >= b->pMin.x) &&
-               (a->pMin.y <= b->pMax.y && a->pMax.y >= b->pMin.y);
+bool CPT_hitTestRectAabb(const RectAabb *a, const RectAabb *b)
+{
+	return (a->pMin.x <= b->pMax.x && a->pMax.x >= b->pMin.x)
+	       && (a->pMin.y <= b->pMax.y && a->pMax.y >= b->pMin.y);
 }
 
-bool CPT_hitTestPointRectAabb(const struct V2* pt, const RectAabb* rect) {
-        return (pt->x >= rect->pMin.x && pt->x <= rect->pMax.x) &&
-               (pt->y >= rect->pMin.y && pt->y <= rect->pMax.y);
+bool CPT_hitTestPointRectAabb(const struct V2 *pt, const RectAabb *rect)
+{
+	return (pt->x >= rect->pMin.x && pt->x <= rect->pMax.x)
+	       && (pt->y >= rect->pMin.y && pt->y <= rect->pMax.y);
 }
 
 // https://math.stackexchange.com/questions/198764/how-to-know-if-a-point-is-inside-a-circle
-bool CPT_hitTestPointCircAabb(const struct V2* pt, const CircAabb* circ) {
-        return SQUARE(SQUARE(pt->x - circ->center.x) +
-                      SQUARE(pt->y - circ->center.y)) <= SQUARE(circ->radius);
+bool CPT_hitTestPointCircAabb(const struct V2 *pt, const CircAabb *circ)
+{
+	return SQUARE(SQUARE(pt->x - circ->center.x)
+		      + SQUARE(pt->y - circ->center.y))
+	       <= SQUARE(circ->radius);
 }
 
 // http://cgp.wikidot.com/circle-to-circle-collision-detection
-bool CPT_hitTestCircAabb(const CircAabb* a, const CircAabb* b) {
-        float dx = a->center.x - b->center.x;
-        float dy = a->center.y - b->center.y;
+bool CPT_hitTestCircAabb(const CircAabb *a, const CircAabb *b)
+{
+	float dx = a->center.x - b->center.x;
+	float dy = a->center.y - b->center.y;
 
-        float radii = a->radius + b->radius;
+	float radii = a->radius + b->radius;
 
-        return (SQUARE(dx) + SQUARE(dy)) < SQUARE(radii);
+	return (SQUARE(dx) + SQUARE(dy)) < SQUARE(radii);
 }
+
+// -----------------------------------------
+//    Macro definitinos
+// -----------------------------------------
+
+VECTOR_DEFINE(RectAabb, RectAabb)
+COMPONENT_MANAGER_DEFINE(RectAabb, RectAabb, RectAabb)
+
+VECTOR_DEFINE(CircAabb, CircAabb)
+COMPONENT_MANAGER_DEFINE(CircAabb, CircAabb, CircAabb)
--- Src/Engine/Components/Aabb.h
+++ Src/Engine/Components/Aabb.h
@@ -4,69 +4,77 @@
 #include "Lib/GenericComponentManager.h"
 #include "Lib/V2.h"
 // -----------------------------------------
-//    Data for Aabbs
+//    Macros for the data to create Aabbs
 // -----------------------------------------
 
 // useful links:
 // https://jaredponn.github.io/2018/06/07/Write-Me-A-FlappyBird-In-Haskell.html
 // https://developer.mozilla.org/kab/docs/Games/Techniques/2D_collision_detection
 
-// declares the default Rect and Circle Aabb which will becasted tofor every
-// type
+#define CREATE_RECTAABB(prefix)                                                \
+	/*RectAabb - used for detecting collisions of rectangles*/             \
+	typedef struct prefix##RectAabb {                                      \
+		struct V2 pMin;                                                \
+		struct V2 pMax;                                                \
+	} prefix##RectAabb;
 
-/*RectAabb - used for detecting collisions of rectangles*/
-typedef struct RectAabb {
-        struct V2 pMin;
-        struct V2 pMax;
-} RectAabb;
+#define CREATE_CIRCAABB(prefix)                                                \
+	/*CircAabb - used for detecting collisions of circles*/                \
+	typedef struct prefix##CircAabb {                                      \
+		struct V2 center;                                              \
+		float radius;                                                  \
+	} prefix##CircAabb;
 
-/*CircAabb - used for detecting collisions of circles*/
-typedef struct CircAabb {
-        struct V2 center;
-        float radius;
-} CircAabb;
-
-/*SegAabb - used for detecting collisions of a line segment*/
-typedef struct SegAabb {
-        struct V2 pMin;
-        struct V2 pMax;
-} SegAabb;
+#define CREATE_SEGAABB(prefix)                                                 \
+	/*SegAabb - used for detecting collisions of a line segment*/          \
+	typedef struct prefix##SegAabb {                                       \
+		struct V2 pMin;                                                \
+		struct V2 pMax;                                                \
+	} prefix##SegAabb;
 
 // -----------------------------------------
-//    final aabb type thats super inefficent, but less complicated and easier to
-//    understand and implement for plebians
+//    Creating the default Aabb types
 // -----------------------------------------
-typedef enum CPT_AabbType { yRECT_AABB, CIRC_AABB, SEG_AABB } CPT_AabbType;
-typedef enum CPT_CollisionLayers {
-        et,
-} CPT_CollisionLayers;
+CREATE_RECTAABB()
+CREATE_CIRCAABB()
 
-typedef struct Aabb {
-        uint8_t aabbType : 4;
-        uint8_t collisionLayer : 4;
-        union {
-                RectAabb rectAabb;
-                CircAabb circAabb;
-                SegAabb segAabb;
-        };
-} Aabb;
+// -----------------------------------------
+//    Defining the sepcialized types for the CPT_Componeents
+// -----------------------------------------
+CREATE_RECTAABB(A)
+CREATE_RECTAABB(B)
+
+// assertions
+_Static_assert(sizeof(RectAabb) == sizeof(ARectAabb),
+	       "error - aabb data types are not the same size");
+_Static_assert(sizeof(RectAabb) == sizeof(BRectAabb),
+	       "error - aabb data types are not the same size");
+// -----------------------------------------
+//    Creating the defaul AABB managers that everything will cast to
+// -----------------------------------------
+
+VECTOR_DECLARE(RectAabb, RectAabb)
+COMPONENT_MANAGER_DECLARE(RectAabb, RectAabb, RectAabb)
+
+VECTOR_DECLARE(CircAabb, CircAabb)
+COMPONENT_MANAGER_DECLARE(CircAabb, CircAabb, CircAabb)
 
 // -----------------------------------------
 //    Hit test procedures
 // -----------------------------------------
 
 // returns true if the the RectAabbs are overlapping
-bool CPT_hitTestRectAabb(const RectAabb*, const RectAabb*);
+bool CPT_hitTestRectAabb(const RectAabb *, const RectAabb *);
 
 // returns true if the point is within the RectAabb
-bool CPT_hitTestPointRectAabb(const struct V2*, const RectAabb*);
+bool CPT_hitTestPointRectAabb(const struct V2 *, const RectAabb *);
 
 // returns true if the the RectAabbs are overlapping
-bool CPT_hitTestCircAabb(const CircAabb*, const CircAabb*);
+bool CPT_hitTestCircAabb(const CircAabb *, const CircAabb *);
 
 // returns true if th epoint is within the circle
-bool CPT_hitTestPointCircAabb(const struct V2*, const CircAabb*);
+bool CPT_hitTestPointCircAabb(const struct V2 *, const CircAabb *);
 
 // returns true if the a circle Aabb and Rect aabb areoverlapping
 // TODO actually not entirely sure how to go about writinh this
-bool CPT_hitTestCircRectAabb(const CircAabb*, const RectAabb*);
+bool CPT_hitTestCircRectAabb(const CircAabb *, const RectAabb *);
